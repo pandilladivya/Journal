f543e79965a714e30e057e283a466ec23a989fea
Divya Pandilla <divya.pandilla8@gmail.com>
2016-12-30 17:53:11 +0530

    my Commit

---
+++ app/src/main/java/sandiv/com/newsapp/DashboardFragment.java
@@ -0,0 +1,21 @@
+package sandiv.com.newsapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+
+	public DashboardFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+         
+        return rootView;
+    }
+}
