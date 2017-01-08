3185e00d09d183177c383f6314362fc9984e8131
yvss.santosh <yvss.santosh@gmail.com>
2017-01-08 18:52:33 +0530

    Added firebase database reference

--- app/src/main/java/sandiv/com/newsapp/DashboardFragment.java
+++ app/src/main/java/sandiv/com/newsapp/DashboardFragment.java
@@ -6,17 +6,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DashboardFragment extends Fragment {
 
+    private EditText mInputData;
+    private Button mInputDataSubmit;
 	public DashboardFragment(){}
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
- 
         View rootView = inflater.inflate(R.layout.activity_main, container, false);
+        mInputData = (EditText) rootView.findViewById(R.id.input_data);
+        mInputDataSubmit = (Button) rootView.findViewById(R.id.input_data_submit);
 
+        mInputDataSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("articles");
+                //
+                myRef.setValue(mInputData.getText().toString());
+            }
+        });
         return rootView;
     }
 
